ba40a915070a90016beb68c13edd712084a1d4d5
artiCodes <133800811+codArtico@users.noreply.github.com>
2024-05-23 20:53:54 -0300

    Deletado: Diretório extra sem utilidade

--- locadoraJava/locadoraJava/src/locadoraJava/RegistrarAlugueis.java
+++
@@ -1,8 +0,0 @@
-package locadoraJava;
-
-import java.util.Scanner;
-
-public interface RegistrarAlugueis {
-	public void devolverCarro(Cliente cliente);
-	public void alugarCarro(Cliente cliente, Scanner sc);
-}
--- locadoraJava/locadoraJava/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module locadoraJava {
-	requires java.desktop;
-}
